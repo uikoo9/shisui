b32b9f74e370c290ad0e2d2c54c9844955e0ed25
uikoo9 <uikoo9@qq.com>
2015-08-13 15:08:33 +0800

    拍照和相册

--- @/js/lib/app/index.js
+++ @/js/lib/app/index.js
@@ -5,9 +5,43 @@ mui.init({
 // 所有方法都放到这里
 mui.plusReady(function(){
 	setColor("#f7f7f7");
+	
+	qiao.on('#choiceImg', 'tap', choiceImg);
 });
 
 // set color
 function setColor(color){
 	if(mui.os.ios && color) plus.navigator.setStatusBarBackground(color);
+}
+
+function choiceImg(){
+	qiao.h.sheet('选择照片', ['拍照','相册'], function(e){
+		switch (e.index){
+			case 0:
+				break;
+			case 1:
+				choiceCamera();
+				break;
+			case 2:
+				choicePic();
+				break;
+		}
+	});
+}
+function choiceCamera(){
+	var cmr = plus.camera.getCamera();
+	cmr.captureImage( function ( p ) {
+		plus.io.resolveLocalFileSystemURL( p, function ( entry ) {
+			
+		}, function ( e ) {
+		} );
+	}, function ( e ) {
+	}, {filename:"_doc/camera/",index:1} );
+}
+function choicePic(){
+	plus.gallery.pick( function(path){
+    	console.log(path);
+    }, function ( e ) {
+    	console.log( "取消选择图片" );
+    }, {filter:"image"} );
 }
