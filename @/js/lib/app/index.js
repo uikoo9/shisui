5aa4d40cdffe6b566bb7beb45d305cf7bab42e99
uikoo9 <uikoo9@qq.com>
2015-08-13 17:23:23 +0800

    上传

--- @/js/lib/app/index.js
+++ @/js/lib/app/index.js
@@ -6,10 +6,10 @@ mui.init({
 mui.plusReady(function(){
 	if(mui.os.ios) plus.navigator.setStatusBarBackground('#EFEFF4');	
 
-	qiao.on('#choiceImg', 'tap', choiceImg);
-	qiao.on('#choiceBtn', 'tap', facepp);
+	qiao.on('#faceImg', 'tap', choiceImg);
+	qiao.on('#faceBtn', 'tap', uploadImg);
 	
-	qiao.qiniu.uptoken();
+	$('#faceForm input[name="token"]').val(qiao.qiniu.uptoken());
 });
 
 // choice img
@@ -32,36 +32,43 @@ function choicePic(){
 	plus.gallery.pick(function(path){setImg(path);},function(e){},{filter:'image'});
 }
 function setImg(src){
-	$('#choiceImg').attr('src', src);
+	$('#faceImg').attr('src', src);
 }
 
-// face pp
-function facepp(){
-	var src = $('#choiceImg').attr('src');
+// uploadImg
+function uploadImg(){
+	var src = $('#faceImg').attr('src');
 	if(src){
 		qiao.h.waiting();
-	    var api = new FacePP('3bbeeac39cd5e8600d2cb05ac97f15fd', '4lf9qM6e7GVLVAfKYITYx9R7GX6_5Taa');
-	    api.request('detection/detect', {
-//	      url: 'http://7sbn90.com1.z0.glb.clouddn.com/@/img/me/face1.jpg',
-	      img : src,
-	      attribute: 'gender,age'
-	    }, function(err, result) {
-	    	qiao.h.closeWaiting();
-			if(err){
-				showRes('识别失败，请重试！');
-				showRes(JSON.stringify(err))
-				return;
-			}
-			  
-			if(result && result.face && result.face.length){
-				var face = result.face[0].attribute;
-				var str = '性别：' + (face.gender.value == 'Male' ? '男':'女') + '，年龄：' + face.age.value;
-				showRes(str);
-			}
-	    });
+		
+		$('#faceForm input[name="file"]').val(src);
+		$('#faceForm').submit(function(e){
+			console.log(JSON.stringify(e));
+//			facepp(url);
+		});
 	}else{
 		showRes('请先选择要识别的照片！');
 	}
+}
+function facepp(url){
+    var api = new FacePP('3bbeeac39cd5e8600d2cb05ac97f15fd', '4lf9qM6e7GVLVAfKYITYx9R7GX6_5Taa');
+    api.request('detection/detect', {
+      url: url,
+      attribute: 'gender,age'
+    }, function(err, result) {
+    	qiao.h.closeWaiting();
+		if(err){
+			showRes('识别失败，请重试！');
+			showRes(JSON.stringify(err))
+			return;
+		}
+		  
+		if(result && result.face && result.face.length){
+			var face = result.face[0].attribute;
+			var str = '性别：' + (face.gender.value == 'Male' ? '男':'女') + '，年龄：' + face.age.value;
+			showRes(str);
+		}
+    });
 }
 function showRes(msg){
 	$('#res').text(msg);
